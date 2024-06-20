901eab6c6f85831f89b6edf2dee66ec69053da7a
Matheus Castro <matheus.castroks@gmail.com>
2024-06-20 19:54:22 -0300

    remove google tag manager

--- scripts/google.js
+++ scripts/google.js
@@ -1,25 +1,9 @@
-(function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
-    "gtm.start": new Date().getTime(),
-    event: "gtm.js",
-  });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != "dataLayer" ? "&l=" + l : "";
-  j.async = true;
-
-  if (localStorage.getItem("cookieConsent") === "accepted") {
-    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-    f.parentNode.insertBefore(j, f);
-  }
-})(window, document, "script", "dataLayer", "GTM-K5XMDN7J");
-
 if (localStorage.getItem("cookieConsent") === "accepted") {
   window.dataLayer = window.dataLayer || [];
   function gtag() {
     dataLayer.push(arguments);
   }
+
   gtag("js", new Date());
 
   gtag("config", "G-FY4CFN7VWX");
